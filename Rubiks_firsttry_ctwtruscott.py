f180d470d6a071f83f984e53addd71df6fc92507
Charles Truscott Watters <119706319+disabledmathematician@users.noreply.github.com>
2024-01-01 12:55:39 +1100

    Update Rubiks_firsttry_ctwtruscott.py

--- Rubiks_firsttry_ctwtruscott.py
+++ Rubiks_firsttry_ctwtruscott.py
@@ -76,23 +76,39 @@ def CharlesTruscott():
 	DF = [["Y", "Y"], ["Y", "Y"]]
 	RubiksCube = [FF, RF, BF, LF, UF, DF]
 	print_state(RubiksCube)
-	print("Rotate left closest facing side upward once")
-	HL_2_1(RubiksCube)
-	print_state(RubiksCube)
-	print("Then three more times for its original state")
-	HL_2_3(RubiksCube)
-	print_state(RubiksCube)
-	print("Turn the left closest side upward twice and the left furthest side upward twice")
-	HL_2_2(RubiksCube)
-	HL_1_2(RubiksCube)
-	print_state(RubiksCube)
-	print("Then turn the left closest side once")
-	HL_2_1(RubiksCube)
-	print_state(RubiksCube)
-	print("Then turn the left furthest side once")
-	HL_1_1(RubiksCube)
-	print_state(RubiksCube)
-	
+#	print("Rotate left closest facing side upward once")
+#	HL_2_1(RubiksCube)
+#	print_state(RubiksCube)
+#	print("Then three more times for its original state")
+#	HL_2_3(RubiksCube)
+#	print_state(RubiksCube)
+#	print("Turn the left closest side upward twice and the left furthest side upward twice")
+#	HL_2_2(RubiksCube)
+#	HL_1_2(RubiksCube)
+#	print_state(RubiksCube)
+#	print("Then turn the left closest side once")
+#	HL_2_1(RubiksCube)
+#	print_state(RubiksCube)
+#	print("Then turn the left furthest side once")
+#	HL_1_1(RubiksCube)
+#	print_state(RubiksCube)
+	answer = ""
+	while answer != str("quit"):
+		print("Enter LFFU or LFCU for left face furthest left face closest upward followed by the amount of rotations, i.e. LFFU1, LFCU3 (to rotate the first supported moves of the cube")
+		answer = str(input())
+		if answer == str("LFFU1"):
+			HL_1_1(RubiksCube)
+		elif answer == str("LFFU2"):
+			HL_1_2(RubiksCube)
+		elif answer == str("LFFU3"):
+			HL_1_3(RubiksCube)
+		elif answer == str("LFCU1"):
+			HL_2_1(RubiksCube)
+		elif answer == str("LFCU2"):
+			HL_2_2(RubiksCube)
+		elif answer == str("LFCU3"):
+			HL_2_3(RubiksCube)
+		print_state(RubiksCube)
 CharlesTruscott()
 
 
@@ -103,7 +119,26 @@ CharlesTruscott()
  Upper Face: [['W', 'W'], ['W', 'W']]
  Down Face: [['Y', 'Y'], ['Y', 'Y']]
 
-Rotate left closest facing side upward once
+Enter LFFU or LFCU for left face furthest left face closest upward followed by the amount of rotations, i.e. LFFU1, LFCU3 (to rotate the first supported moves of the cube
+LFFU1
+Front Face: [['G', 'G'], ['G', 'G']]
+ Right Face: [['R', 'W'], ['R', 'W']]
+ Left Face: [['Y', 'O'], ['Y', 'O']]
+ Back Face: [['B', 'B'], ['B', 'B']]
+ Upper Face: [['O', 'O'], ['W', 'W']]
+ Down Face: [['R', 'R'], ['Y', 'Y']]
+
+Enter LFFU or LFCU for left face furthest left face closest upward followed by the amount of rotations, i.e. LFFU1, LFCU3 (to rotate the first supported moves of the cube
+LFCU1
+Front Face: [['G', 'G'], ['G', 'G']]
+ Right Face: [['W', 'W'], ['W', 'W']]
+ Left Face: [['Y', 'Y'], ['Y', 'Y']]
+ Back Face: [['B', 'B'], ['B', 'B']]
+ Upper Face: [['O', 'O'], ['O', 'O']]
+ Down Face: [['R', 'R'], ['R', 'R']]
+
+Enter LFFU or LFCU for left face furthest left face closest upward followed by the amount of rotations, i.e. LFFU1, LFCU3 (to rotate the first supported moves of the cube
+LFFU3
 Front Face: [['G', 'G'], ['G', 'G']]
  Right Face: [['W', 'R'], ['W', 'R']]
  Left Face: [['O', 'Y'], ['O', 'Y']]
@@ -111,7 +146,8 @@ Front Face: [['G', 'G'], ['G', 'G']]
  Upper Face: [['W', 'W'], ['O', 'O']]
  Down Face: [['Y', 'Y'], ['R', 'R']]
 
-Then three more times for its original state
+Enter LFFU or LFCU for left face furthest left face closest upward followed by the amount of rotations, i.e. LFFU1, LFCU3 (to rotate the first supported moves of the cube
+LFCU3
 Front Face: [['G', 'G'], ['G', 'G']]
  Right Face: [['R', 'R'], ['R', 'R']]
  Left Face: [['O', 'O'], ['O', 'O']]
@@ -119,7 +155,17 @@ Front Face: [['G', 'G'], ['G', 'G']]
  Upper Face: [['W', 'W'], ['W', 'W']]
  Down Face: [['Y', 'Y'], ['Y', 'Y']]
 
-Turn the left closest side upward twice and the left furthest side upward twice
+Enter LFFU or LFCU for left face furthest left face closest upward followed by the amount of rotations, i.e. LFFU1, LFCU3 (to rotate the first supported moves of the cube
+LFFU2
+Front Face: [['G', 'G'], ['G', 'G']]
+ Right Face: [['R', 'O'], ['R', 'O']]
+ Left Face: [['R', 'O'], ['R', 'O']]
+ Back Face: [['B', 'B'], ['B', 'B']]
+ Upper Face: [['Y', 'Y'], ['W', 'W']]
+ Down Face: [['W', 'W'], ['Y', 'Y']]
+
+Enter LFFU or LFCU for left face furthest left face closest upward followed by the amount of rotations, i.e. LFFU1, LFCU3 (to rotate the first supported moves of the cube
+LFCU2
 Front Face: [['G', 'G'], ['G', 'G']]
  Right Face: [['O', 'O'], ['O', 'O']]
  Left Face: [['R', 'R'], ['R', 'R']]
@@ -127,24 +173,20 @@ Front Face: [['G', 'G'], ['G', 'G']]
  Upper Face: [['Y', 'Y'], ['Y', 'Y']]
  Down Face: [['W', 'W'], ['W', 'W']]
 
-Then turn the left closest side once
+Enter LFFU or LFCU for left face furthest left face closest upward followed by the amount of rotations, i.e. LFFU1, LFCU3 (to rotate the first supported moves of the cube
+quit
 Front Face: [['G', 'G'], ['G', 'G']]
- Right Face: [['Y', 'O'], ['Y', 'O']]
- Left Face: [['R', 'W'], ['R', 'W']]
+ Right Face: [['O', 'O'], ['O', 'O']]
+ Left Face: [['R', 'R'], ['R', 'R']]
  Back Face: [['B', 'B'], ['B', 'B']]
- Upper Face: [['Y', 'Y'], ['R', 'R']]
- Down Face: [['W', 'W'], ['O', 'O']]
-
-Then turn the left furthest side once
-Front Face: [['G', 'G'], ['G', 'G']]
- Right Face: [['Y', 'Y'], ['Y', 'Y']]
- Left Face: [['W', 'W'], ['W', 'W']]
- Back Face: [['B', 'B'], ['B', 'B']]
- Upper Face: [['R', 'R'], ['R', 'R']]
- Down Face: [['O', 'O'], ['O', 'O']]
+ Upper Face: [['Y', 'Y'], ['Y', 'Y']]
+ Down Face: [['W', 'W'], ['W', 'W']]
 
 
 [Program finished]
 """
 
 # Thank you Eric Grimson, John Guttag, Ana Bell and MITx and MIT OCW
+
+# Nice stay in Tweed Centre for Mental Health Kurrajong Unit from November 23rd to December 22nd. Friendly nurses, great roast beef, great roast lamb and potato, great beef rending, butter chicken, soap washes, 9 hours sleep, 8:30 a.m. wake up, 12 30 p.m. lunch and 5:30 p.m. dinner and sunshine and coffee and tea and fruit and prunes.
+
